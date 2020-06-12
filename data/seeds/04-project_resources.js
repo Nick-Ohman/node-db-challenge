431715e6d3221fc96fc4a45f7d1c62804047f205
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-06-12 11:33:12 -0500

    seeds ran after some fixing

--- data/seeds/04-project_resources.js
+++ data/seeds/04-project_resources.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('project_resources').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('project_resources').insert([
         {id: 1, project_id: 1, resource_id: 1},
         {id: 2, project_id: 1, resource_id: 2},
         {id: 3, project_id: 1, resource_id: 3},
